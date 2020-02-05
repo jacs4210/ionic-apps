807c292e57cf2c40b2e3cafd5a4be86743cd2b4d
Jairo Cuartas <jairo.cuartas18@gmail.com>
2020-02-05 18:22:52 +0000

    Corrección de definición de objetos y propiedades.

--- photo-gallery/src/app/services/photo.service.ts
+++ photo-gallery/src/app/services/photo.service.ts
@@ -45,7 +45,7 @@ export class PhotoService {
                         delete photoCopy.base64;
 
                         return photoCopy;
-                    }));
+                    }))
         });
     }
 
@@ -140,7 +140,7 @@ export class PhotoService {
             for (let photo of this.photos) {
             // Read each saved photo's data from the Filesystem
             const readFile = await Filesystem.readFile({
-                path: photo.filepath,
+                path: photo.filePath,
                 directory: FilesystemDirectory.Data
             });
 
